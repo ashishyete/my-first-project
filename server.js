7b4b476dba43a339ab1bf818f041b8560fa4e9df
Ashish <ashish.yete@centurylink.com>
2021-06-17 12:39:32 +0530

    committed

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the angularapp directory
-app.use(express.static(process.cwd()+'/my-first-project'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/my-first-project'));
 
 app.get('/*', function(req,res) {
-
-res.sendFile(path.join(process.cwd(),'/my-first-project/index.html'));
+    
+res.sendFile(path.join(__dirname+'/dist/my-first-project/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
